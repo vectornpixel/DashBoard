252918fc18e0fea52ce680633283eb1d1bcdfeba
vectornpixel <vectornpixel@gmail.com>
2013-11-07 14:05:17 -0500

    sort with numbers

--- Site/assets/js/bigjquery.js
+++ Site/assets/js/bigjquery.js
@@ -2,20 +2,68 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-       $(document).ready( function() {
-
-        $( '#cd-dropdown' ).dropdown({
-        stack : false
-        });
-        $( '#type' ).dropdown({
-        stack : false
-        });
-        $( '#sort' ).dropdown({
-        stack : false
-        });
-
+    // Sortable Showcase Javascript
+    
+   $(document).ready(function(){
+     // drop down forms
+    $( '#cd-dropdown' ).dropdown({
+    stack : false
     });
-    $(".collapse").collapse();
 
+     //sortable 
     $('.sortable').sortable();
 
+    $('.handles').sortable({
+            handle: 'span'
+    });
+    $('.connected').sortable({
+            connectWith: '.connected'
+    });
+    $('.exclude').sortable({
+            items: ':not(.disabled)'
+    });
+
+    // Display Numbers
+   
+  
+   
+   
+   
+   // Button Icons
+   var icon1 = $('.sortable').find('li').append('<span class="delete glyphicon glyphicon-remove showcase-icons"></span>');
+   var icon2 = $('.sortable').find('li').append('<span class="glyphicon glyphicon-pencil showcase-icons2 pull-right"></span>');
+   
+
+   // add new content type
+   $('.add').click(function(){ 
+       
+   // clones the content buttons
+   var liitem = $(this).last().clone();
+   $('ul.sortable').append(liitem);
+
+   // refresh sort list
+   $('.sortable').sortable('refresh');
+
+   // after buttons are cloned and added, it removes the add icons and add the edit icons
+   $('.sortable').find('li span').each(function(){
+    if ($(this).hasClass('glyphicon glyphicon-plus showcase-icons')){
+        $(this).removeClass("glyphicon glyphicon-plus showcase-icons").addClass('delete glyphicon glyphicon-remove showcase-icons').after('<span class="glyphicon glyphicon-pencil showcase-icons2 pull-right"></span>');  
+         }
+       });
+    });
+   
+   
+   //remove sort buttons
+   $('.delete').click(function(){
+       $(this).parent().remove(".btn-sort");
+   }); 
+
+   
+   
+ });
+                
+                
+                
+                
+                
+     
